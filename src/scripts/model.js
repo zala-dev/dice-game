da9c72343e0c1c37885fd769703ba7141dc48f71
Zala <zala.developer@gmail.com>
2024-03-31 12:47:52 +1030

    move model functionality to a separate model js file

---
+++ src/scripts/model.js
@@ -0,0 +1,15 @@
+/* Constant */
+//the total number of rounds in the game
+const TOTAL_ROUNDS = 3;
+
+/* Model */
+//the model of the game, containing various game state properties.
+const model = {
+  // Intial state of the game model
+  activePlayer: "player",
+  playerCurrentScore: 0,
+  playerRoundWon: 0,
+  computerCurrentScore: 0,
+  computerRoundWon: 0,
+  currentRoundNumber: 1,
+};
